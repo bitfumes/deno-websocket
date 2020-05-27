93847e065abe35f2eafae6e4ea5bc50a497e3ecb
sarthaksavvy <bitfumes@gmail.com>
2020-05-28 00:48:38 +0530

    refactored, Handle Http Connections

--- main.ts
+++ main.ts
@@ -2,34 +2,43 @@ import { serve } from "https://deno.land/std/http/server.ts";
 import {
   acceptWebSocket,
   isWebSocketCloseEvent,
-  isWebSocketPingEvent,
+  acceptable,
   WebSocket,
 } from "https://deno.land/std/ws/mod.ts";
 
-const connections = new Array<WebSocket>();
 async function main() {
   console.log("Server is started at http://localhost:8080");
   for await (const req of serve("localhost:8080")) {
-    const { conn, headers, w: bufWriter, r: bufReader } = req;
-    acceptWebSocket({ conn, headers, bufReader, bufWriter }).then(
-      async (ws: WebSocket): Promise<void> => {
-        console.log("Websocker connection stablished");
-        connections.push(ws);
-        console.log(connections.length);
-        for await (const event of ws) {
-          if (typeof event === "string") {
-            for (const websocket of connections) {
-              if (websocket !== ws) {
-                websocket.send(event);
-              }
-            }
-          }
-          if (isWebSocketCloseEvent(event)) {
-            console.log("WebSocket connection closed");
-          }
-        }
-      }
-    );
+    if (acceptable(req)) {
+      const { conn, headers, w: bufWriter, r: bufReader } = req;
+      acceptWebSocket({ conn, headers, bufReader, bufWriter }).then(
+        handleWebSocket
+      );
+    } else {
+      req.respond({ body: "Not Found", status: 404 });
+    }
+  }
+}
+
+const connections = new Array<WebSocket>();
+async function handleWebSocket(ws: WebSocket): Promise<void> {
+  console.log("Websocket connection stablished");
+  connections.push(ws);
+  for await (const event of ws) {
+    if (typeof event === "string") {
+      broadcastEvents(ws, event);
+    }
+    if (isWebSocketCloseEvent(event)) {
+      console.log("WebSocket connection closed");
+    }
+  }
+}
+
+function broadcastEvents(ws: WebSocket, event: string) {
+  for (const websocket of connections) {
+    if (websocket !== ws) {
+      websocket.send(event);
+    }
   }
 }
 
