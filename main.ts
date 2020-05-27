eed8a6c89eef4c41809d2d7c145e1ce4a32f21ac
sarthaksavvy <bitfumes@gmail.com>
2020-05-27 19:17:34 +0530

    establish websocket connection

---
+++ main.ts
@@ -0,0 +1,24 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+import {
+  acceptWebSocket,
+  isWebSocketCloseEvent,
+  isWebSocketPingEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
+
+async function main() {
+  console.log("Server is started at http://localhost:8080");
+  for await (const req of serve("localhost:8080")) {
+    const { conn, headers, w: bufWriter, r: bufReader } = req;
+    acceptWebSocket({ conn, headers, bufReader, bufWriter }).then(
+      async (ws: WebSocket): Promise<void> => {
+        console.log("Websocker connection stablished");
+        for await (const event of ws) {
+          console.log(event);
+        }
+      }
+    );
+  }
+}
+
+main();
