19328bcc65738d86f0d6f8561416690cb66184bf
sarthaksavvy <bitfumes@gmail.com>
2020-06-03 23:27:09 +0530

    refactor main file

---
+++ WebSocket/index.ts
@@ -0,0 +1,66 @@
+import {
+  isWebSocketCloseEvent,
+  WebSocket,
+} from "https://deno.land/std/ws/mod.ts";
+
+interface Connection {
+  name: string;
+  ws: WebSocket;
+}
+
+const connections = new Array<Connection>();
+export default async function handleWebSocket(ws: WebSocket): Promise<void> {
+  console.log("Websocket connection stablished");
+  for await (const event of ws) {
+    if (typeof event === "string") {
+      const data = JSON.parse(event);
+      if (data.type === "register") {
+        handleRegister(connections, ws, data);
+      } else {
+        broadcastEvents(ws, event);
+      }
+    }
+    if (isWebSocketCloseEvent(event)) {
+      handleClose(ws, connections);
+    }
+  }
+}
+
+function handleRegister(
+  connections: Array<Connection>,
+  ws: WebSocket,
+  data: { name: string; type: string }
+) {
+  connections.push({ name: data.name, ws });
+  ws.send(`${data.name}, you are registered`);
+  const onlineUsers = JSON.stringify({
+    type: "online",
+    message: { users: connections.map((connection) => connection.name) },
+  });
+  ws.send(onlineUsers);
+  const ev = JSON.stringify({
+    type: "join",
+    message: { name: data.name },
+  });
+  broadcastEvents(ws, ev);
+}
+
+function handleClose(ws: WebSocket, connections: Array<Connection>) {
+  console.log("WebSocket connection closed");
+  const currentConn = connections.filter((c) => c.ws == ws);
+  if (currentConn.length == 1) {
+    const ev = JSON.stringify({
+      type: "left",
+      message: { name: currentConn[0].name },
+    });
+    broadcastEvents(ws, ev);
+  }
+  connections.splice(connections.indexOf(currentConn[0]), 1);
+}
+function broadcastEvents(ws: WebSocket, event: string) {
+  for (const connection of connections) {
+    if (connection.ws !== ws) {
+      connection.ws.send(event);
+    }
+  }
+}
--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-import {
-  acceptWebSocket,
-  isWebSocketCloseEvent,
-  acceptable,
-  WebSocket,
-} from "https://deno.land/std/ws/mod.ts";
+import handleWebSocket from "./WebSocket/index.ts";
+import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
 
 async function main() {
   console.log("Server is started at http://localhost:8080");
@@ -25,52 +21,6 @@ async function main() {
       } else {
         req.respond({ body: "Not Found", status: 404 });
       }
-    }
-  }
-}
-
-const connections = new Array<{ name: string; ws: WebSocket }>();
-async function handleWebSocket(ws: WebSocket): Promise<void> {
-  console.log("Websocket connection stablished");
-  for await (const event of ws) {
-    if (typeof event === "string") {
-      const data = JSON.parse(event);
-      if (data.type === "register") {
-        connections.push({ name: data.name, ws });
-        ws.send(`${data.name}, you are registered`);
-        const onlineUsers = JSON.stringify({
-          type: "online",
-          message: { users: connections.map((connection) => connection.name) },
-        });
-        ws.send(onlineUsers);
-        const ev = JSON.stringify({
-          type: "join",
-          message: { name: data.name },
-        });
-        broadcastEvents(ws, ev);
-      } else {
-        broadcastEvents(ws, event);
-      }
-    }
-    if (isWebSocketCloseEvent(event)) {
-      console.log("WebSocket connection closed");
-      const currentConn = connections.filter((c) => c.ws == ws);
-      if (currentConn.length == 1) {
-        const ev = JSON.stringify({
-          type: "leaved",
-          message: { name: currentConn[0].name },
-        });
-        broadcastEvents(ws, ev);
-      }
-      connections.splice(connections.indexOf(currentConn[0]), 1);
-    }
-  }
-}
-
-function broadcastEvents(ws: WebSocket, event: string) {
-  for (const connection of connections) {
-    if (connection.ws !== ws) {
-      connection.ws.send(event);
     }
   }
 }
